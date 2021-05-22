52ce8590f15be6576195b0230e22a23f918ab534
dan-primo <danielly.primo@gmail.com>
2021-05-22 16:37:08 -0300

    testes

--- API/Models/Context.cs
+++ API/Models/Context.cs
@@ -12,7 +12,7 @@ namespace API.Models
         public DbSet<Produto> Produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(connectionString: );
+            optionsBuilder.UseSqlServer(connectionString: @"endereço-do-db");
         }
     }
 }
